446909319622426279bfe51b96b062716a76d01f
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-12-27 13:00:33 +0900

    refact

--- index.ts
+++ index.ts
@@ -1,23 +1,22 @@
 import { Client } from '@notionhq/client'
 import { AbstractDatabase, RelationField, TextField } from './src'
 
-const client = new Client({
-  auth: process.env.NOTION_TOKEN,
-})
-
-const id = process.env.INPUT_DATABASE_ID ?? ''
 export class Architecture extends AbstractDatabase<Architecture> {
-  title: TextField
-  category: RelationField
+  title = new TextField('title')
+  category = new RelationField('category')
 
+  // biome-ignore lint/complexity/noUselessConstructor: <explanation>
   constructor(client: Client, id: string) {
     super(client, id)
-    this.title = new TextField('title')
-    this.category = new RelationField('category')
   }
 }
 
 async function main() {
+  const client = new Client({
+    auth: process.env.NOTION_TOKEN,
+  })
+
+  const id = process.env.INPUT_DATABASE_ID ?? ''
   const architecture = new Architecture(client, id)
   const results = await architecture.findBy({
     where: {
