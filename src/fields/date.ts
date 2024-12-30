f985717635beb6c5ba5385795c0015416aba9b24
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-12-30 13:06:34 +0900

    add date

---
+++ src/fields/date.ts
@@ -0,0 +1,47 @@
+import { BaseField, type FillValue } from '@/fields/base'
+
+export type DateFieldEquals = { property: string; date: { equals: string } }
+export type DateFieldBefore = { property: string; date: { before: string } }
+export type DateFieldAfter = { property: string; date: { after: string } }
+export type DateFieldOnOrBefore = { property: string; date: { on_or_before: string } }
+export type DateFieldOnOrAfter = { property: string; date: { on_or_after: string } }
+
+export class DateField extends BaseField<'date'> {
+  constructor(readonly property: string) {
+    super()
+  }
+
+  fill(value: {
+    start: string
+    end: string | null
+  }): FillValue<'date'> {
+    return { date: { ...value, time_zone: 'Asia/Tokyo' } }
+  }
+
+  equals(value: string): DateFieldEquals {
+    return { property: this.property, date: { equals: value } }
+  }
+
+  before(value: string): DateFieldBefore {
+    return { property: this.property, date: { before: value } }
+  }
+
+  after(value: string): DateFieldAfter {
+    return { property: this.property, date: { after: value } }
+  }
+
+  onOrBefore(value: string): DateFieldOnOrBefore {
+    return { property: this.property, date: { on_or_before: value } }
+  }
+
+  onOrAfter(value: string): DateFieldOnOrAfter {
+    return { property: this.property, date: { on_or_after: value } }
+  }
+}
+
+export type DateFieldCondition =
+  | DateFieldEquals
+  | DateFieldBefore
+  | DateFieldAfter
+  | DateFieldOnOrBefore
+  | DateFieldOnOrAfter
