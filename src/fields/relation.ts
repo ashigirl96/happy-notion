ed94c6c517efcd34c41aa0973d8cd432b3caefaf
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-12-30 13:29:39 +0900

    fix relation

--- src/fields/relation.ts
+++ src/fields/relation.ts
@@ -1,74 +1,39 @@
-export type RelationFieldContains = {
-  property: string
-  relation: {
-    title: {
-      contains: string
-    }
-  }
-}
+import { BaseField, type FillValue } from '@/fields/base'
+
+export type RelationFieldContains = { property: string; relation: { contains: string } }
 export type RelationFieldDoesNotContain = {
   property: string
-  relation: {
-    title: {
-      does_not_contain: string
-    }
-  }
+  relation: { does_not_contain: string }
 }
-export type RelationFieldIsEmpty = {
-  property: string
-  relation: {
-    is_empty: true
+export type RelationFieldIsEmpty = { property: string; relation: { is_empty: true } }
+export type RelationFieldIsNotEmpty = { property: string; relation: { is_not_empty: true } }
+
+export class RelationField extends BaseField<'relation'> {
+  constructor(readonly property: string) {
+    super()
   }
-}
-export type RelationFieldIsNotEmpty = {
-  property: string
-  relation: {
-    is_not_empty: true
+
+  fill(id: string): FillValue<'relation'> {
+    return { relation: [{ id }] }
   }
-}
-export class RelationField {
-  constructor(readonly content: string) {}
 
   contains(value: string): RelationFieldContains {
-    return {
-      property: this.content,
-      relation: {
-        title: {
-          contains: value,
-        },
-      },
-    }
+    return { property: this.property, relation: { contains: value } }
   }
 
   doesNotContain(value: string): RelationFieldDoesNotContain {
-    return {
-      property: this.content,
-      relation: {
-        title: {
-          does_not_contain: value,
-        },
-      },
-    }
+    return { property: this.property, relation: { does_not_contain: value } }
   }
 
   isEmpty(): RelationFieldIsEmpty {
-    return {
-      property: this.content,
-      relation: {
-        is_empty: true,
-      },
-    }
+    return { property: this.property, relation: { is_empty: true } }
   }
 
   isNotEmpty(): RelationFieldIsNotEmpty {
-    return {
-      property: this.content,
-      relation: {
-        is_not_empty: true,
-      },
-    }
+    return { property: this.property, relation: { is_not_empty: true } }
   }
 }
+
 export type RelationFieldCondition =
   | RelationFieldContains
   | RelationFieldDoesNotContain
