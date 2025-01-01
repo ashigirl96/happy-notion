7b78af86e34cac692d46514d0088303a9d1a697a
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2025-01-01 13:16:45 +0900

    delete

--- src/fields/base.ts
+++ src/fields/base.ts
@@ -14,15 +14,6 @@ export type FillValue<T> = CreatePageParameters['properties'] extends Record<str
     : never
   : never
 
-// // export type MapValue = CreatePageParameters['properties'] extends Record<string, infer U>
-// //   ? U extends { type?: Kind }
-// //   : never
-// type MapValue = CreatePageParameters['properties'] extends Record<string, infer U>
-//   ? U extends { type?: string }
-//     ? U
-//     : never
-//   : never
-
 export abstract class BaseField<K extends Kind> {
   protected constructor() {}
 
